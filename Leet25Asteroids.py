4de037685d43b267b7bc8eef97d2b481064de955
Amirreza Jafarian <amirreza.jfn2000@gmail.com>
2025-04-03 22:11:55 +0300

    final commit

--- Leet25Asteroids.py
+++ Leet25Asteroids.py
@@ -1,23 +1,23 @@
 class Solution:
     def asteroidCollision(self, asteroids: list[int]) -> list[int]:
-
+        
         stack = []
-        for asteroid in asteroids:
-            while stack and asteroid < 0 < stack[-1]:
-                if stack[-1] < -asteroid:
-                    stack.pop()
-                    continue
-                elif stack[-1] == -asteroid:
-                    stack.pop()
-                break
+
+        for a in asteroids:
+            while stack and a < 0 < stack[-1]:
+                if abs(a) > abs(stack[-1]):
+                    stack.pop()  
+                elif abs(a) == abs(stack[-1]):
+                    stack.pop() 
+                    break
+                else:
+                    break  
             else:
-                stack.append(asteroid)
+                stack.append(a)
+
         return stack
-        
 
 
-
-
+# Example usage
 solution = Solution()
-print(solution.asteroidCollision([-2,-1,1,2]))
-        
+print(solution.asteroidCollision([-2, -1, 1, 2]))  
